d00c7841ba3a1eff33ec49c6f5c9589585f389d2
SoloDevAbu <abubakkar2502@gmail.com>
2024-12-29 12:10:05 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Features from "./components/Features"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero/>
+        <Features/>
+      </div>
+    </>
+  )
+}
+
+export default App
